5a7c875ed71d17db0980c890afb6fd4ede7c47db
Gurpreet Raju <gurpreetraj92@gmail.com>
2025-03-09 12:49:13 -0400

    List view improvements

---
+++ Shine.Components.Demo/Data/NavConfig.cs
@@ -0,0 +1,18 @@
+﻿namespace Shine.Components.Demo.Data
+{
+    /// <summary>
+    /// The navigation item config.
+    /// </summary>
+    public record NavConfig
+    {
+        /// <summary>
+        /// Name of the page.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The link.
+        /// </summary>
+        public string Link { get; set; }
+    }
+}
