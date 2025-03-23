a727899312bda1a5f3f2bd2ee76d1c75c8866652
Gurpreet Raju <gurpreetraj92@gmail.com>
2025-03-23 17:27:53 -0400

    changed the demo project to web assembly app

--- ShineBlazor.Components.Demo/Program.cs
+++ ShineBlazor.Components.Demo/Program.cs
@@ -1,31 +1,19 @@
 using ShineBlazor.Components.Demo;
 using ShineBlazor.Components.Services;
 
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddSingleton<DummyDataProvider>();
 builder.Services.AddScoped<ToastService>();
 
-var app = builder.Build();
+await builder.Build().RunAsync();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 
-app.UseHttpsRedirection();
 
-app.UseAntiforgery();
-
-app.MapStaticAssets();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
